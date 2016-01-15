29ae34c8ffe3d9f4b8ce522e6b625478f965d7b8
lamalice31 <terry.blain31@gmail.com>
2016-01-15 15:13:47 +0100

    add endpoint communication class

--- app/src/main/java/com/m2dl/helloandroid/wastelocator/HomeActivity.java
+++ app/src/main/java/com/m2dl/helloandroid/wastelocator/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.m2dl.helloandroid.wastelocator;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Pair;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
     }
 }
