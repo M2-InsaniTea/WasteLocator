b8c38270fd0369cc29f4e0ca196b0435124f4f80
lamalice31 <terry.blain31@gmail.com>
2016-01-22 14:36:52 +0100

    Map displaying test (center on BMIG)

--- app/src/main/java/com/m2dl/helloandroid/wastelocator/MapsActivity.java
+++ app/src/main/java/com/m2dl/helloandroid/wastelocator/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng bmig = new LatLng(43.562070,1.466741);
+        mMap.addMarker(new MarkerOptions().position(bmig).title("BMIG"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bmig));
     }
 }
