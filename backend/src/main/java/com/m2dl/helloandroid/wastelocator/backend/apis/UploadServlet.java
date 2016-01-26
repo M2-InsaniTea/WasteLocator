afd428c90c991db93ba121b9f433bc237a33ccd0
meGAmeS1 <florentin.lemoal@gmail.com>
2016-01-26 19:37:31 +0100

    First try to upload file

---
+++ backend/src/main/java/com/m2dl/helloandroid/wastelocator/backend/apis/UploadServlet.java
@@ -0,0 +1,37 @@
+package com.m2dl.helloandroid.wastelocator.backend.apis;
+
+/**
+ * Created by flemoal on 26/01/16.
+ */
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+public class UploadServlet extends HttpServlet {
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
+        List<BlobKey> blobKeys = blobs.get("myFile");
+
+        if (blobKeys == null || blobKeys.isEmpty()) {
+            res.sendRedirect("/");
+        } else {
+            res.sendRedirect("/serve?blob-key=" + blobKeys.get(0).getKeyString());
+        }
+    }
+}
+
