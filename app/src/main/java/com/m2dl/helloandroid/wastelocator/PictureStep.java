7924f8cc64b7712b46b380c7410e2375347e45db
meGAmeS1 <florentin.lemoal@gmail.com>
2016-01-27 11:54:02 +0100

    Revert "icomplete geloc feature using Fused location API"
    This reverts commit f2dd59a2ea6af25638e612609dd769ecf8072925.

--- app/src/main/java/com/m2dl/helloandroid/wastelocator/PictureStep.java
+++ app/src/main/java/com/m2dl/helloandroid/wastelocator/PictureStep.java
@@ -1,17 +1,17 @@
 package com.m2dl.helloandroid.wastelocator;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,11 +20,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.m2dl.helloandroid.wastelocator.backend.wasteApi.model.Tag;
 
 import org.codepond.wizardroid.WizardStep;
@@ -34,7 +29,7 @@ import java.io.File;
 import java.util.List;
 
 
-public class PictureStep extends WizardStep implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+public class PictureStep extends WizardStep implements LocationListener {
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private Uri imageUri;
     private ImageView imageView;
@@ -44,16 +39,6 @@ public class PictureStep extends WizardStep implements GoogleApiClient.Connectio
 
     private Double longitude;
     private Double latitude;
-
-    private static final String TAG = PictureStep.class.getSimpleName();
-
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
-
-    private Location mLastLocation;
-
-    // Google client to interact with Google API
-    private GoogleApiClient mGoogleApiClient;
-
 
     public PictureStep() {
         // Required empty public constructor
@@ -74,11 +59,9 @@ public class PictureStep extends WizardStep implements GoogleApiClient.Connectio
             }
         });
 
-        if (checkPlayServices()) {
-
-            // Building the GoogleApi client
-            buildGoogleApiClient();
-        }
+        LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        //noinspection ResourceType
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         return v;
     }
@@ -87,7 +70,7 @@ public class PictureStep extends WizardStep implements GoogleApiClient.Connectio
     public void takePhoto(View view) {
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
         imageUri = Uri.fromFile(photo);
 
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
@@ -136,98 +119,25 @@ public class PictureStep extends WizardStep implements GoogleApiClient.Connectio
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
-                + result.getErrorCode());
-    }
-
-    @Override
-    public void onConnected(Bundle arg0) {
-
-        // Once connected with google api, get the location
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-    }
-
-    @Override
-    public void onConnectionSuspended(int arg0) {
-        mGoogleApiClient.connect();
-    }
-
-    private void displayLocation(Location location) {
-
-        //noinspection ResourceType
-        mLastLocation = location;
-
-        if (mLastLocation != null) {
-            latitude = mLastLocation.getLatitude();
-            longitude = mLastLocation.getLongitude();
-
-            Toast.makeText(getContext(),"I got you location. watch out!!",Toast.LENGTH_LONG).show();
-
-        } else {
-
-            Log.i(TAG, "location is null");
-        }
-    }
-
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API).build();
-    }
-
-    private boolean checkPlayServices() {
-        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
-        int result = googleAPI.isGooglePlayServicesAvailable(getContext());
-        if(result != ConnectionResult.SUCCESS) {
-            if(googleAPI.isUserResolvableError(result)) {
-                googleAPI.getErrorDialog(getActivity(), result,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            }
-
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        checkPlayServices();
-    }
-
-    @Override
     public void onLocationChanged(Location location) {
-        displayLocation(location);
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+
+        Toast.makeText(getContext(),"Location acquired, we can see you",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
